bd140a0acfa357c69a4cfc956e86ccd9e2d17857
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2026-02-23 00:26:13 +0700

    Add agentation mcp

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {process.env.NODE_ENV === 'development' && (
+          <Agentation endpoint='http://localhost:4747' />
+        )}
       </body>
     </html>
   );
